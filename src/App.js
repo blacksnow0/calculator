7eef6ec3ecc841eb6f0210996d809e452a066b05
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2024-07-16 15:32:57 +0530

    Added the app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    const value = Number(inputRef.current.value);
+    if (!isNaN(value)) {
+      setResult((result) => result + value);
+    }
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  function minus(e) {
+    e.preventDefault();
+    const value = Number(inputRef.current.value);
+    if (!isNaN(value)) {
+      setResult((result) => result - value);
+    }
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  function times(e) {
+    e.preventDefault();
+    const value = Number(inputRef.current.value);
+    if (!isNaN(value)) {
+      setResult((result) => result * value);
+    }
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    const value = Number(inputRef.current.value);
+    if (!isNaN(value) && value !== 0) {
+      setResult((result) => result / value);
+    }
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input ref={inputRef} type="number" placeholder="Type a number" />
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Minus</button>
+        <button onClick={times}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form>
     </div>
   );
 }
